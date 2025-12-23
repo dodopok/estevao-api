3b04764dbf7d02c67361e7912c0b8a7d86277111
Douglas Araujo <dodopokcamilo@gmail.com>
2025-12-23 15:39:56 -0300

    feat: adding solid cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_19_235257) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_23_183708) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -374,6 +374,17 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_19_235257) do
     t.index ["expires_at"], name: "index_shared_offices_on_expires_at"
     t.index ["short_code"], name: "index_shared_offices_on_short_code", unique: true
     t.index ["user_id"], name: "index_shared_offices_on_user_id"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.integer "byte_size", null: false
+    t.datetime "created_at", null: false
+    t.binary "key", null: false
+    t.bigint "key_hash", null: false
+    t.binary "value", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "user_onboardings", force: :cascade do |t|
