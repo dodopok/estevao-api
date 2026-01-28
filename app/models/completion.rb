582225a9caddba3d2f6a251dad3a55f90c848b25
Douglas Araujo <dodopokcamilo@gmail.com>
2026-01-28 09:57:55 -0300

    fix: temporarily remove completion validation

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -4,10 +4,6 @@ class Completion < ApplicationRecord
   belongs_to :user
 
   validates :date_reference, presence: true
-  validates :office_type, presence: true, inclusion: {
-    in: %w[morning midday evening compline],
-    message: "%<value>s is not a valid office type"
-  }
   validates :user_id, uniqueness: {
     scope: %i[date_reference office_type],
     message: "already completed this office today"
